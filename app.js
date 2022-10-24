cd50f5f3a09efdcaae42e467ee907a1c15fcfa7c
10 <tochka098@gmail.com>
2022-10-24 17:14:46 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Selectors
+const todoInput = document.querySelector(".todo-form__input");
+const todoButton = document.querySelector(".todo-form__button");
+const todo = document.querySelector(".todo");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  // prevent form submition
+  event.preventDefault();
+
+  // create .todo__list div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo__item");
+
+  // create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "this is inner text of li";
+  newTodo.classList.add("todo__text");
+  todoDiv.appendChild(newTodo);
+
+  // check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("todo__complete-button");
+  todoDiv.appendChild(completedButton);
+
+  // trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("todo__trash-button");
+  todoDiv.appendChild(trashButton);
+
+  // Append to <ul class="todo"></ul>
+  todo.appendChild(todoDiv);
+}
